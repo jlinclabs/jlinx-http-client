56b337403b4918f9db9bd2de43cf9d52af9da1fc
Jared Grippe <jared@deadlyicon.com>
2022-05-11 16:39:35 -0700

    renamed npm package jlinx-client -> jlinx-http-client

--- index.js
+++ index.js
@@ -1,110 +1,96 @@
-// import Path from 'path'
-// import ini from 'ini'
-// import fs from 'fs/promises'
+import Debug from 'debug'
+import http from 'node:http'
+import https from 'node:https'
+import fetch from 'node-fetch'
+import { URL } from 'url'
+import { createSigningKeyPair, keyToBuffer, keyToDid, didToKey } from 'jlinx-core/util.js'
 
-// import Debug from 'debug'
-// const debug = Debug('jlinx:client')
-// // import Didstore from './Didstore.js'
-// // import JlinxServer from 'jlinx-server'
-// import KeyStore from 'jlinx-core/KeyStore.js'
-// // import DidDocument from './DidDocument.js'
-// import { keyToString, createSigningKeyPair, fsExists, } from 'jlinx-core/util.js'
+const debug = Debug('jlinx:remoteAgent')
 
-// /*
-//  * ~/.jlinx
-//  * ~/.jlinx/config
-//  * ~/.jlinx/cores
-//  * ~/.jlinx/dids
-//  * ~/.jlinx/dids/${did} (containts did document)
-//  * ~/.jlinx/keys/${publicKeyAsUrlSafeMd5} (contains private key)
-//  *
-//  * new JlinxClient()
-//  * new JlinxClient({
-//  *   storagePath: '/somewhere/else',
-//  *   server: new JlinxRemoteServer({
-//  *      url: 'https://dids.jlinx.io',
-//  *   })
-//  * })
-//  */
-// export default class JlinxClient {
+const options = {
+  agent: function(_parsedURL) {
+    if (_parsedURL.protocol == 'http:') {
+      return httpAgent;
+    } else {
+      return httpsAgent;
+    }
+  }
+};
 
-//   // static get defaultStoragePath(){
-//   //   return Path.join(os.homedir(), '.jlinx')
-//   // }
+export default class JlinxHttpClient {
 
-//   constructor(opts){
-//     this.storagePath = opts.storagePath
-//     if (!this.storagePath) throw new Error(`JlinxClient requires a server`)
+  constructor(url, opts = {}){
+    this.url = url
+    this.httpAgent = new http.Agent({ keepAlive: true })
+    this.httpsAgent = new https.Agent({ keepAlive: true })
+  }
 
-//     this.configPath = Path.join(this.storagePath, 'config.ini')
+  [Symbol.for('nodejs.util.inspect.custom')](depth, opts){
+    let indent = ''
+    if (typeof opts.indentationLvl === 'number')
+      while (indent.length < opts.indentationLvl) indent += ' '
+    return this.constructor.name + '(\n' +
+      indent + '  url: ' + opts.stylize(this.url, 'string') + '\n' +
+      // indent + '  cores: ' + opts.stylize(this.corestore.cores.size, 'number') + '\n' +
+      // indent + '  writable: ' + opts.stylize(this.writable, 'boolean') + '\n' +
+      indent + ')'
+  }
 
-//     // used to store crypto keys locally
-//     this.keyStore = new KeyStore({
-//       storagePath: Path.join(this.storagePath, 'keys')
-//     })
-
-//     // instead of taking a server make one for each action?
+  async ready(){
+    if (this._ready) this._ready = (async () => {
 
 
-//     this.servers = [
-//       RemoteJlinxServer({ url: 'http://jlinx.io' })
-//     ]
-//     // this.server = opts.server
-//     // if (!this.server) throw new Error(`JlinxClient requires a server`)
+    })()
+    return await this._ready
+  }
+
+  async connected(){
+    return await this.ready()
+  }
+
+  async destroy(){
+    this.httpAgent.destroy()
+  }
+
+  async fetch(path, options){
+    const url = new URL(`${this.url}${path}`)
+    options.agent = this.httpAgent
+    options.headers = {
+      Accepts: 'application/json',
+      ...options.headers
+    }
+    debug('FETCH', url, options)
+    const response = await fetch(url, options)
+    debug({ response })
+    return await response.json()
+  }
+
+  async getJSON(path){
+    return this.fetch(path, { method: 'get' })
+  }
+
+  async postJSON(path, body){
+    return this.fetch(path, {
+      method: 'post',
+      body: JSON.stringify(body),
+      headers: {'Content-Type': 'application/json'}
+    })
+  }
+
+  async resolveDid(did){
+
+  }
+
+  async createDid(){
+    return await this.postJSON('/new', {})
+  }
+
+  async amendDid({did, secret, value}){
+    return await this.postJSON(`/${did}`, {
+      secret, didDocument: value
+    })
+  }
 
 
-//     //   opts.server ||
-//     //   // TODO check config.ini for server options
-//     //   new JlinxHttpServer({
-//     //     url: 'https://jlinx.io',
-//     //     // storagePath: this.storagePath,
-//     //     // keyStore: this.keyStore,
-//     //   })
-//     // )
-//   }
-
-//   [Symbol.for('nodejs.util.inspect.custom')](depth, opts){
-//     let indent = ''
-//     if (typeof opts.indentationLvl === 'number')
-//       while (indent.length < opts.indentationLvl) indent += ' '
-//     return this.constructor.name + '(\n' +
-//       indent + '  storagePath: ' + opts.stylize(this.storagePath, 'string') + '\n' +
-//       indent + '  configPath: ' + opts.stylize(this.configPath, 'string') + '\n' +
-//       // indent + '  size: ' + opts.stylize(this.size, 'number') + '\n' +
-//       // indent + '  writable: ' + opts.stylize(this.writable, 'boolean') + '\n' +
-//       indent + ')'
-//   }
-
-//   async resolveDid(did){
-//     // await this.ready()
-
-//     // we could resolve the did via
-//     // A) hyperswarm directly
-//     // B) N http jlinx servers
-
-//     const results = await Promise.all(
-//       this.servers.map(server =>
-//         server.resolveDid(did)
-//       )
-//     )
-//     console.log(results)
-//     return await this.server.resolveDid(did)
-//   }
-
-//   async createDid(){
-//     const { did } = await this.server.createDid()
-//     console.log({ did })
-//     const signingKeyPair = await this.keyStore.createSigningKeyPair()
-//     const encryptingKeyPair = await this.keyStore.createEncryptingKeyPair()
-//     const value = generateDidDocument({
-//       did,
-//       signingPublicKey: signingKeyPair.publicKey,
-//       encryptingPublicKey: encryptingKeyPair.publicKey,
-//     })
-//     await this.server.updateDid(did, value)
-//     console.log({ did, value })
-//     const didDocument = await this.server.resolveDid(didDocument.did)
-//     return didDocument
-//   }
-// }
+}
 
